a835c1c2c188198dac24633fdf1c32b60713dbe6
Gregory Kronhardt <gregorykronhardt@NTCNOTE73.ntconsult.net>
2021-05-03 11:40:22 -0300

    Desabilitar o scheduling ao executar os testes unitários

--- src/test/java/br/com/agibank/directorywatcherservice/DirectoryWatcherServiceApplicationTests.java
+++ src/test/java/br/com/agibank/directorywatcherservice/DirectoryWatcherServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package br.com.agibank.directorywatcherservice;
 
+import br.com.agibank.directorywatcherservice.service.DirectoryWatcherService;
+import br.com.agibank.directorywatcherservice.service.FileProcessorService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
+@MockBean(FileProcessorService.class)
+@MockBean(DirectoryWatcherService.class)
 class DirectoryWatcherServiceApplicationTests {
 
 	@Test
